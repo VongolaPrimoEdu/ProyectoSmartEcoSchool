729e7100e572fe2f26f2429d09ba3af3e30c2d4f
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-02-20 17:04:24 +0100

    Mayoría de la plantilla de la UI acabada

---
+++ resources/views/layouts/ui.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Plantilla de UI de Smartecoschool</title>
+	<link rel="stylesheet" href="{{asset("css/app.css")}}">
+	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+<body>
+	<header>
+		<img id="logo" src="{{asset("img/logo.png")}}" alt="Logotipo de Smartecoschool">
+		<div class="swiper-container">
+			<p>CONSUMO</p>
+			<div class="swiper-wrapper">
+				<div class="swiper-slide">
+					<p class="consume-type">DIARIO</p>
+					{{-- <img src="{{asset("img/icons/arrow.svg")}}" alt="" style="width:20%;"> --}}
+				</div>
+				<div class="swiper-slide">
+					<p class="consume-type">SEMANAL</p>
+				</div>
+				<div class="swiper-slide">
+					<p class="consume-type">MENSUAL</p>
+				</div>
+				<div class="swiper-slide">
+					<p class="consume-type">PORCENTAJES</p>
+				</div>
+			</div>
+		</div>
+		<h1>@yield("title")</h1>
+	</header>
+	<main>
+			<img src="{{asset("img/icons/water.svg")}}" alt="Gota de agua">
+			<img src="{{asset("img/icons/lightning.svg")}}" alt="Rayo de electricidad">
+			<div>
+				<canvas id="consumo-agua"></canvas>
+			</div>
+			<div>
+				<canvas id="consumo-electricidad"></canvas>
+			</div>
+	</main>
+</body>
+</html>
