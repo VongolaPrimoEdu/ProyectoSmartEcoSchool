e16d70a9e0bd9c98c6a0d8ef285ef3fb29a4d1de
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-02-25 17:23:55 +0100

    Terminada vista con consumo respecto de los 7 días previos al actual

--- app/Http/Controllers/UIController.php
+++ app/Http/Controllers/UIController.php
@@ -15,34 +15,26 @@ class UIController extends Controller
 	public function __construct() {
 		$this->fecha_aleatoria = Carbon::create(2020, 2, 10);
 	}
-
+	/**
+	 * La función "daily" recupera y almacena datos de consumo diario de electricidad y agua durante la
+	 * última semana para mostrarlos en una vista.
+	 * 
+	 * @return La función `daily()` está devolviendo una vista llamada 'ui.daily' con los datos
+	 * almacenados en el array `$consumo_por_dia`. Esta matriz contiene los datos de consumo de los
+	 * últimos 7 días desglosados por día de la semana y tipo de sensor (electricidad y agua).
+	 */
 	public function daily()
 	{
-
-	}
-	
-  public function weekly()
-	{	
-    // Obtener la fecha actual
-    // $fecha_actual = Carbon::now();
-
     // Arreglo para almacenar los datos de consumo por día de la semana
     $consumo_por_dia = [];
-		//la fecha es 2020-04-16
     // Recorrer los últimos 7 días
     for ($i = 7; $i > 0; $i--) {
         // Obtener la fecha para el día actual menos $i días
 				$fecha_reducida = $this->fecha_aleatoria->copy()->subDays($i);
         $fecha = $fecha_reducida->toDateString();
-
-        // Obtener los datos de consumo por hora para esa fecha
-        $consumo_por_hora_electricidad = $this->getConsumoDelDia(1, $fecha); // Tipo de sensor para electricidad: 1
-        $consumo_por_hora_agua = $this->getConsumoDelDia(2, $fecha); // Tipo de sensor para agua: 2
-
-        // Sumar el consumo total por hora para obtener el consumo diario
-        $consumo_diario_electricidad = $consumo_por_hora_electricidad;
-        $consumo_diario_agua = $consumo_por_hora_agua;
-
+        // Obtener el consumo del día actual con respecto al de hace $i días
+        $consumo_diario_electricidad = $this->getConsumoDelDia(1, $fecha); // Tipo de sensor para electricidad: 1
+        $consumo_diario_agua = $this->getConsumoDelDia(2, $fecha); // Tipo de sensor para agua: 2
         // Almacenar los datos en el arreglo por día de la semana
         $dia_semana = $fecha_reducida->dayName;
         $consumo_por_dia[$dia_semana] = [
@@ -50,7 +42,11 @@ class UIController extends Controller
             'electricidad' => $consumo_diario_electricidad,
         ];
     }
-    return view('ui.weekly', compact('consumo_por_dia'));
+    return view('ui.daily', compact('consumo_por_dia'));
+	}
+	
+  public function weekly()
+	{
 		// // Obtener fecha.
 		// $inicio_2020 = Carbon::create(2020, 2, 1); // 1 de enero de 2020
 		// $final_2020 = Carbon::create(2020, 7, 31);   // 31 de julio de 2020
@@ -79,7 +75,6 @@ class UIController extends Controller
 		// 		'agua' => $porcentaje_agua,
 		// 	];
 		// }
-	
 		// return view('ui.currentday', compact('porcentajes'));
 	}
 	
@@ -129,19 +124,30 @@ class UIController extends Controller
 		}
 		return $consumo_por_hora;
 	}
-
-	private function getConsumoDelDia($id_tipo_sensor, $fecha)
+	
+	private function getConsumoDelDia($id_tipo_sensor, $fecha_relativa)
 	{
 		$sensor_type = DB::select("SELECT id_sensor FROM sensors WHERE id_type=$id_tipo_sensor");
-		$consumo_del_dia = DB::table('measurements')
-		->select(DB::raw("consumo"))
-		->whereIn('id_sensor', collect($sensor_type)->pluck('id_sensor')->toArray())
-		->whereRaw('DATE(fecha) = ?', [$fecha])
-		->orderByDesc('consumo')
-		->limit(1)
-		->pluck('consumo')
+
+		$ultimo_registro_dia_previo = DB::table("measurements")->select(DB::raw("consumo"))
+		->whereIn("id_sensor", collect($sensor_type)->pluck("id_sensor")->toArray())->
+		whereRaw("DATE(fecha) = ?", [$fecha_relativa])->orderByDesc("consumo")->limit(1)->pluck("consumo")
 		->get(0);
-		return $consumo_del_dia;
+		$primer_registro_dia_previo = DB::table("measurements")->select(DB::raw("consumo"))
+		->whereIn("id_sensor", collect($sensor_type)->pluck("id_sensor")->toArray())->
+		whereRaw("DATE(fecha) = ?", [$fecha_relativa])->orderBy("consumo")->limit(1)->pluck("consumo")
+		->get(0);
+		$ultimo_registro_dia_actual = DB::table("measurements")->select(DB::raw("consumo"))
+		->whereIn("id_sensor", collect($sensor_type)->pluck("id_sensor")->toArray())
+		->whereRaw("DATE(fecha) = ?", [$this->fecha_aleatoria->toDateString()])->orderByDesc("consumo")
+		->limit(1)->pluck("consumo")->get(0);
+		$primer_registro_dia_actual = DB::table("measurements")->select(DB::raw("consumo"))
+		->whereIn("id_sensor", collect($sensor_type)->pluck("id_sensor")->toArray())
+		->whereRaw("DATE(fecha) = ?", [$this->fecha_aleatoria->toDateString()])->orderBy("consumo")
+		->limit(1)->pluck("consumo")->get(0);
+		$consumo_dia_actual = $ultimo_registro_dia_actual - $primer_registro_dia_actual;
+		$consumo_dia_previo = $ultimo_registro_dia_previo - $primer_registro_dia_previo;
+		return $consumo_dia_actual - $consumo_dia_previo;
 	}
 
 	private function getConsumoTotalSemana($id_tipo_sensor, $fecha)
--- resources/views/ui/daily.blade.php
+++ resources/views/ui/daily.blade.php
@@ -1,9 +1,45 @@
 @extends("layouts.ui")
 @section("title","Consumo diario.")
-@section("subtitle","Gráficas que reflejan cuánta agua y electricidad han sido consumidas durante este día.")
+@section("subtitle","Gráficas que reflejan cuánta agua y electricidad han sido consumidas durante este día con respecto a los 7 días anteriores.")
 @section("objwater")
+{
+	type: 'line',
+	data: {
+		labels: ['L','M','X','J','V','S','D'],
+		datasets: [
+			{
+				label: "",
+				data: [
+					@foreach ($consumo_por_dia as $consumo_dia)
+						{{$consumo_dia["agua"]}},
+					@endforeach
+				],
+				borderColor: 'rgb(0,0,255)',
+				backgroundColor: 'rgba(0,0,255,0.5)'
+			}
+		]
+	}
+}
 @endsection
 @section("objelec")
+{
+	type: 'line',
+	data: {
+		labels: ['L','M','X','J','V','S','D'],
+		datasets: [
+			{
+				label: "",
+				data: [
+					@foreach ($consumo_por_dia as $consumo_dia)
+						{{$consumo_dia["electricidad"]}},
+					@endforeach
+				],
+				borderColor: 'rgb(143,143,0)',
+				backgroundColor: 'rgba(143,143,0,0.5)'
+			}
+		]
+	}
+}
 @endsection
 @section("next-location", route("ui.weekly"))
 @section("time",20000)
